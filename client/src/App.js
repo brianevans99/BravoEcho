a3b2e1d83dc6ab98f3ddf5d14d39ec54bb28587c
Brian Evans <brianevans99@gmail.com>
2019-10-27 20:51:23 -0400

    setup React

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import Navbar from "./components/Navbar/index";
+import Header from "./components/Header/index";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Header/index.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import logo from "../../assets/images/BRAVO_ECHO_COLOR_TRANSPARENT-min.png";
+
+export default class Header extends Component {
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-4 d-flex justify-content-center" id="belogo">
+                        <img src={logo} className="App-logo" alt="logo" />
+                    </div>
+                    <div className="col-lg-8 d-flex justify-content-center align-items-center">
+                        <div className="animated fadeIn slower">
+                            <span><h1 className="text-center">Affordable Web Presence</h1></span>
+                            <h5 className="text-center">for Small Businesses and Organizations</h5>
+                        </div>
+                    </div>
+                </div>
+        
+        
+            </div>
+        
+        )
+    }
+}
