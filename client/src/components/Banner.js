d458285033e5c55fa55bbee93ce4b46cca6db1db
Brian Evans <brianevans99@gmail.com>
2020-04-24 18:11:23 -0400

    added video background to react

---
+++ client/src/components/Banner.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import logo from '../assets/images/BRAVO_ECHO_COLOR_TRANSPARENT-min.png'
+import styled from 'styled-components'
+
+const Styles = styled.div``
+
+export default class Banner extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <div className='row'>
+          <div className='col-lg-4 d-flex justify-content-center' id='belogo'>
+            <img src={logo} className='App-logo' alt='logo' />
+          </div>
+          <div className='col-lg-8 d-flex justify-content-center align-items-center'>
+            <div className='animated fadeIn slower'>
+              <span>
+                <h1 className='text-center'>Affordable Web Presence</h1>
+              </span>
+              <h5 className='text-center'>
+                for Small Businesses and Organizations
+              </h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
--- client/src/components/Video.js
+++ client/src/components/Video.js
@@ -1,5 +1,67 @@
 import React from 'react'
+import video from '../assets/videos/BlurredVideoScriptsBeingTyped.mp4'
+import styled from 'styled-components'
+import Banner from './Banner'
+
+const Styles = styled.div`
+  .showcase {
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    color: #fff;
+    padding: 0 20px;
+  }
+  .video-container {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    background: var(--primary-color)
+      url('./https://traversymedia.com/downloads/cover.jpg') no-repeat center
+      center/cover;
+  }
+  .video-container video {
+    min-width: 100%;
+    min-height: 100%;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    object-fit: cover;
+  }
+  .video-container:after {
+    content: '';
+    z-index: 1;
+    height: 100%;
+    width: 100%;
+    top: 0;
+    left: 0;
+    background: rgba(0, 0, 0, 0.5);
+    position: absolute;
+  }
+  .content {
+    z-index: 2;
+  }
+`
 
 export default function Video() {
-  return <div>Video overlay</div>
+  return (
+    <Styles>
+      <section class='showcase'>
+        <div class='video-container'>
+          {' '}
+          <video autoPlay loop muted>
+            <source src={video} type='video/mp4' />
+          </video>
+        </div>
+        <div class='content'>
+          <Banner />
+        </div>
+      </section>
+    </Styles>
+  )
 }
