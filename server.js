e29974c4fba1be34610ada9ed5e91ec0bb65ec3d
Brian Evans <brianevans99@gmail.com>
2020-04-25 18:47:45 -0400

    prepared server.js and package.json for heroku build

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const path = require('path')
+const app = express()
 // const bodyParser = require('body-parser');
 // const mongoose = require('mongoose');
 // const dataRoutes = express.Router();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
-app.listen(PORT, function() {
-    console.log(`server is listening on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`server is listening on port ${PORT}`))
